c2580b9f53b66fa3422d7f4949a254563b4d0cb4
thundersunny <151111058+thundersunny@users.noreply.github.com>
2023-11-21 11:31:51 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,18 @@
+﻿﻿function handleSubmit () {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname);
+    localStorage.setItem("EMAIL", email); */
+    
+    sessionStorage.setItem("NAME", name);
+    sessionStorage.setItem("SURNAME", surname);
+     sessionStorage.setItem("EMAIL", email);
+
+
+    return;
+}
---
+++ result.js
@@ -0,0 +1,24 @@
+﻿﻿window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname');
+    const email = params.get('email'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME');
+    const email = localStorage.getItem('EMAIL'); */
+    
+    const name = sessionStorage.getItem('NAME');
+    const surname = sessionStorage.getItem('SURNAME');
+    const email = sessionStorage.getItem('EMAIL');
+
+    
+    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-surname').innerHTML = surname;
+    document.getElementById('result-email').innerHTML = email;
+
+
+})
